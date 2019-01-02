17676fb96cd71d127d34e304554ab2ae808a9454
Lewis <rklewis2@ndc.nasa.gov>
2019-01-02 10:07:21 -0800

    test

--- server.js
+++ server.js
@@ -49,13 +49,12 @@ const jwtAuth = passport.authenticate('jwt', { session: false });
 //////////////////
 
 
-app.use(favicon(__dirname + 'Client/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'Client/build/')));
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'Client/build/', 'index.html'));
+app.use(express.static(path.join(__dirname, 'Client/build')));
+
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
+
 
 
 app.get('/teams', (req, res) => {
